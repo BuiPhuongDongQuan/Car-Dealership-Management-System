c097a53b40f0e28e22e067a689a4bc295f9262de
MK1104 <s3978862@rmit.edu.vn>
2024-08-26 19:39:16 +0700

    fixbug

--- src/Roles/User.java
+++ src/Roles/User.java
@@ -32,42 +32,7 @@ public class User {
         this.password = password;
     }
 
-    public User() {
-
-    }
-
-    // Method to read data from the file
-    public void readData() {
-        users.clear();
-
-        int countLine = Features.countLine(user_data);
-        String[] id = Features.ReadCol(0, user_data, ",");
-        String[] fullname = Features.ReadCol(1, user_data, ",");
-        String[] datesOfBirth = Features.ReadCol(2, user_data, ",");
-        String[] address = Features.ReadCol(3, user_data, ",");
-        String[] phoneNumber = Features.ReadCol(4, user_data, ",");
-        String[] email = Features.ReadCol(5, user_data, ",");
-        String[] userType = Features.ReadCol(6, user_data, ",");
-        String[] status = Features.ReadCol(7, user_data, ",");
-        String[] username = Features.ReadCol(8, user_data, ",");
-        String[] password = Features.ReadCol(9, user_data, ",");
-
-        // Check if arrays have the same length
-        for (int i = 0; i < countLine - 1; i++) {
-            users.add(new User(
-                    id[i], fullname[i], datesOfBirth[i], address[i], phoneNumber[i], email[i], userType[i], status[i], username[i], password[i]
-            ));
-        }
-    }
-
-    public User getUser(String username) {
-        for (User user : users) {
-            if (user.getUsername() != null && user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
+    public User() {}
 
     public String getId() {
         return id;
