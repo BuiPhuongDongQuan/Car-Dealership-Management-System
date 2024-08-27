a132256c29353618f9ce014ee23bdeeb9a05fd4c
BuiPhuongDongQuan <s3975939@rmit.edu.vn>
2024-08-27 09:16:36 +0700

    #1-FEAT: Client Register and Login

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,5 +1,6 @@
 package Menu;
 
+import Roles.Client;
 import Roles.Manager;
 
 import java.io.*;
@@ -8,6 +9,7 @@ import java.util.*;
 
 public class Menu {
     static Manager manager = new Manager();
+    static Client client = new Client();
     static Scanner sc = new Scanner(System.in);
     static Scanner keyboard = new Scanner(System.in);
 
@@ -20,7 +22,7 @@ public class Menu {
         return optionInput;
     }
 
-    public static void systemMenu() throws IOException {
+    public static void SystemMenu() throws IOException {
         System.out.println("============================== System Menu - Welcome to Auto316 Management System ==============================");
         System.out.println("Please let us know who you are: ");
         System.out.println("1. Manager");
@@ -32,6 +34,9 @@ public class Menu {
         switch (choice) {
             case 1:
                 ManagerLoginMenu();
+                break;
+            case 3:
+                ClientWelcomeMenu();
                 break;
         }
 
@@ -48,9 +53,95 @@ public class Menu {
     }
 
     // manager menu
-    public static void managerMenu() throws IOException {
+    public static void ManagerMenu() throws IOException {
         System.out.println("============================== Manager - Menu ==============================");
         System.out.println("Welcome admin to Auto316 Management System");
     }
 
+    public static void ClientWelcomeMenu() throws IOException {
+        System.out.println("============================== Client Welcome Menu ==============================");
+        System.out.println("1. Register");
+        System.out.println("2. Login.");
+        System.out.println("3. Return to Store System Screen.");
+        System.out.println("4. Exit.");
+        int option = optionInput();
+
+        switch (option){
+            case 1:
+                CustomerRegisterMenu();
+                break;
+            case 2:
+                ClientLoginMenu();
+                break;
+            case 3:
+                SystemMenu();
+                break;
+            case 4:
+                ThankYouMenu();
+                break;
+        }
+    }
+
+    public static void ClientLoginMenu() throws IOException {
+        System.out.println("============================== Client - Login ==============================");
+        System.out.print("Enter your username: ");
+        String username = sc.nextLine();
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine();
+
+        client.login(username, password);
+    }
+
+    // manager menu
+    public static void ClientMenu() throws IOException {
+        System.out.println("============================== Client - Menu ==============================");
+        System.out.println("Welcome client to Auto316 Management System");
+    }
+
+    //Allow customer to register
+    public static void CustomerRegisterMenu() throws IOException {
+        System.out.println("============================== Client - Register ==============================");
+        System.out.print("Enter your username: ");
+        String username = keyboard.nextLine();
+
+        if(client.usernameValidation(username) == true){
+            System.out.println("Username existed. Moving you back to Customer Welcome Menu");
+            ClientWelcomeMenu();
+        }
+        else{
+            System.out.print("Enter your password: ");
+            String password = keyboard.nextLine();
+
+            System.out.print("Enter your full name: ");
+            String fullname = keyboard.nextLine();
+
+            System.out.print("Enter your date of birth: ");
+            String dateOfBirth = keyboard.nextLine();
+
+            System.out.print("Enter your email: ");
+            String email = keyboard.nextLine();
+
+            System.out.print("Enter your address: ");
+            String address = keyboard.nextLine();
+
+            System.out.print("Enter your phone number: ");
+            String phoneNumber = keyboard.nextLine();
+
+            client.register(fullname,dateOfBirth,address,phoneNumber,email,username,password);
+
+            System.out.println("Registration completed!");
+            ClientWelcomeMenu();
+        }
+    }
+
+    public static void ThankYouMenu(){
+        System.out.println("============================== Thank you for using our system! Goodbye ==============================");
+        System.out.println("COSC2081 GROUP ASSIGNMENT");
+        System.out.println("AUTO168 CAR DEALERSHIP MANAGEMENT SYSTEM");
+        System.out.println("Instructor: Mr. Minh Vu & Mr. Dung Nguyen");
+        System.out.println("Group: Javalorant :)");
+        System.out.println("s3978862, Nguyen Minh Khoi");
+        System.out.println("s3975939, Bui Phuong Dong Quan");
+        System.out.println("s3980424, Lam Quang Nhat");
+    }
 }
--- src/Roles/Client.java
+++ src/Roles/Client.java
@@ -1,8 +1,86 @@
 package Roles;
+import Features.Features;
+import Menu.Menu;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
-public class Client {
 
+public class Client extends User {
+    private final String user_data = "src/Database/User.txt";
+
+    public Client() {
+        super();
+    }
+    public Client(String id, String fullname, String dateOfBirth, String address, String phoneNumber, String email, String userType, String status, String username, String password) {
+        super(id, fullname, dateOfBirth, address, phoneNumber, email, userType, status, username, password);
+    }
+
+
+    public void login(String username, String password) throws IOException{
+        readData();
+
+        boolean loginValidation = false;
+        User user = new User();
+        user = getUser(username);
+
+        if(user == null){
+            System.out.println("Client not found. Please register to begin.");
+            Menu.SystemMenu();
+        }
+        else{
+            if(user.authenticate(username, password, "Client")){;
+                loginValidation = true;
+            }
+            else {
+                loginValidation = false;
+            }
+
+            if(loginValidation == true){
+                System.out.println("Login success! Welcome back our customer!");
+                Menu.ClientMenu();
+
+            }
+            else{
+                System.out.println("Login failed! Username or password is incorrect.");
+                Menu.ClientLoginMenu();
+            }
+        }
+    }
+
+    public void register(String fullName, String dateOfBirth, String address, String phoneNumber, String email, String username, String password) throws IOException {
+        int countLine = Features.countLine(user_data);
+        String id = "U" + countLine;
+        String user_type = "Client";
+        String status = "Active";
+        String newClient = "\n" + id+ "," + fullName+ "," + dateOfBirth+ "," + address+ "," + phoneNumber+ "," + email+ "," + user_type+ "," + status+ "," + username + "," + password;
+        Features.writeToFile(user_data, newClient);
+    }
+
+    public ArrayList<String> getUsernameArraylist(){
+        String[] usernameArray = Features.ReadCol(6, user_data,",");
+        ArrayList<String> userNameArrayList = new ArrayList<>();
+
+        for(int i = 0; i < usernameArray.length; i++){
+            userNameArrayList.add(usernameArray[i]);
+        }
+
+        return userNameArrayList;
+    }
+
+
+    public boolean usernameValidation(String username){
+        boolean usernameValidation = true;
+        ArrayList<String> allUsernameList = getUsernameArraylist();
+
+        if(allUsernameList.contains(username)) {
+            usernameValidation = true;
+        }
+        else {
+            usernameValidation = false;
+        }
+
+        return usernameValidation;
+    }
 }
+
